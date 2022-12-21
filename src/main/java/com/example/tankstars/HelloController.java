48f7f5bc01c10ff8cf76ee44e808526578347f85
razafimiadana21411 <106375833+Lilia2022@users.noreply.github.com>
2022-12-21 16:43:29 +0530

    INITIAL

--- src/main/java/com/example/tankstars/HelloController.java
+++ src/main/java/com/example/tankstars/HelloController.java
@@ -65,15 +65,8 @@ public class HelloController {
         stage.show();
 
     }
-    /*@FXML
-    private Label welcomeText;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
-
-
+    /*
     @FXML
     void play(MouseEvent e){
         String filename = "";//name of song
